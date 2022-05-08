4dc7ab0acc32d32ae71c88832ed87f0c95735e22
warspyt <tatianax2442@gmail.com>
2022-05-07 20:30:26 -0500

    objeto factura

---
+++ src/Factura.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class Factura {
+    int id_factura;
+    int id_producto;
+    int cantidad;
+    int total;
+    Date fecha;
+
+    public Factura() {
+        this.id_factura = 0;
+        this.id_producto = 0;
+        this.cantidad = 0;
+        this.total = 0;
+        this.fecha = new Date();
+    }
+
+    public Factura(int id_factura, int id_producto, int cantidad, int total, Date fecha) {
+        this.id_factura = 0;
+        this.id_producto = 0;
+        this.cantidad = 0;
+        this.total = 0;
+        this.fecha = new Date();
+    }
+
+    public int getId_factura() {
+        return id_factura;
+    }
+
+    public void setId(int id_factura) {
+        this.id_factura = id_factura;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+}
